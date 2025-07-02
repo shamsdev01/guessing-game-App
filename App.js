aa861fb4903301fd04958d5a909ae3ad87abd183
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-07-02 22:10:31 +0100

    Refactor App component to include ImageBackground for enhanced UI

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet, }from 'react-native';
+import { StyleSheet,ImageBackground }from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <LinearGradient   colors={['#4e0329','#ddb52f']} style={styles.container}>
-       <StartGameScreen/>
+    <ImageBackground
+    source={require('./assets/images/background.png')}
+    resizeMode="cover"
+    style={styles.container}
+    imageStyle={styles.backgroundImage}
+    >
+        <StartGameScreen/>
+    </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,6 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container :{
     flex: 1,
+  },
+  backgroundImage:{
+    opacity: 0.15,
   }
 });
  
